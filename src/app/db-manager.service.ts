1bc8d67b921c338806a415db751b8fb66db78633
Jordan Lat <40663699+jordanlat@users.noreply.github.com>
2023-11-17 17:34:51 +0100

    ajoute get et set firebase

--- src/app/db-manager.service.ts
+++ src/app/db-manager.service.ts
@@ -1,26 +1,53 @@
 import { Injectable } from '@angular/core';
-import { getDatabase, ref, set } from 'firebase/database';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, push, get, orderByValue, limitToFirst, query, orderByChild, orderByKey, limitToLast } from 'firebase/database';
+import { firebaseConfig } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DbManagerService {
 
-  //app = initializeApp(firebaseConfig);
+  app = initializeApp(firebaseConfig);
+  db = getDatabase();
+  arr_lead: any = [];
+
   constructor() { }
 
   async getData() {
+    try {
+      const thisref = ref(this.db, 'leaderboard');
 
+      const fireQuery = query(thisref, orderByChild('score'), limitToLast(4));
+      const query_get = get(fireQuery);
+
+      await query_get.then((data) => {
+        data.forEach((child) => {
+          this.arr_lead.push(child.val());
+        });
+      });
+
+      console.log(this.arr_lead.reverse());
+
+    } catch (error) {
+      console.log("erreur get pseudo");
+      console.log(error);
+    }
   }
 
   setData(thisPseudo: string, thisScore: number) {
-    const db = getDatabase();
-    
-    set(ref(db), {
-      pseudo: thisPseudo,
-      score: thisScore
-    });
+    try {
+      push(ref(this.db, 'leaderboard/'), {
+        "pseudo": thisPseudo,
+        "score": thisScore
+      });
+      this.getData();
 
+    } catch (error) {
+      console.log("erreur set pseudo");
+      console.log(error);
+    }
   }
 
 }
+
